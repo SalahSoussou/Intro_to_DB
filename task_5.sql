a1ddab563ad02a46fac55a69c82a07d8333cf4e8
SalahSoussou <soussousalahIddine@gmail.com>
2024-10-20 20:54:45 +0100

    OK

--- task_5.sql
+++ task_5.sql
@@ -3,6 +3,6 @@ INSERT INTO customer (customer_id ,
 customer_name ,
 email ,
 address ,) VALUES ( 1,
- Cole Baidoo,
- cbaidoo@sandtech.com,
- Happiness Ave);
+ "Cole Baidoo",
+ "cbaidoo@sandtech.com",
+ "123 Happiness Ave");
