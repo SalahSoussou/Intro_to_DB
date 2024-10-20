8a2c1e4ed7643a548d435bffab660b13ebd99208
Salah Eddine Soussou <88331708+SalahSoussou@users.noreply.github.com>
2024-10-20 20:03:35 +0100

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -29,7 +29,7 @@ order_date DATE,
 
 CREATE TABLE Order_Details(
 orderdetailid PRIMARY KEY,
-order_id FOREIGN KEY REFERENCES Orders(order_id),
-book_id FOREIGN KEY REFERENCES Books(book_id),
+order_id INT FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+book_id FOREIGN KEY (book_id) REFERENCES Books(book_id),
 quantity DOUBLE,
 )
